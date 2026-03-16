d99ee695e37dbb353132238311eebb1b45264096
mashraba02-eng <mashraba02@icloud.com>
2026-03-16 14:56:48 +0500

    Update shazam_helper.py

--- shazam_helper.py
+++ shazam_helper.py
@@ -1,1 +1,13 @@
+from shazamio import Shazam
+import os
+
+async def identify_track(file_path):
+    shazam = Shazam()
+    out = await shazam.recognize_song(file_path)
+    if out and 'track' in out:
+        return {
+            'title': out['track']['title'],
+            'author': out['track']['subtitle']
+        }
+    return None
 
